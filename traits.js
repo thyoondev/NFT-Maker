7fdb574b5951d6f2cb9ab84da1c4759fb63dec1b
thyoondev <th.yoon.dev@gmail.com>
2022-03-07 10:06:26 +0900

    이미지 이름 수정

--- traits.js
+++ traits.js
@@ -1,26 +1,26 @@
 const cat = [
-    {id: 01, name: 'cat-one'}
-   ,{id: 02, name: 'cat-two'}
-   ,{id: 03, name: 'cat-three'}
-   ,{id: 04, name: 'cat-four'}
-   ,{id: 05, name: 'cat-five'}
-   ,{id: 06, name: 'cat-six'}
-   ,{id: 07, name: 'cat-luckyone'}
-   ,{id: 08, name: 'cat-eight'}
-   ,{id: 09, name: 'cat-nine'}
+    {id: 1, name: 'cat-one'}
+   ,{id: 2, name: 'cat-two'}
+   ,{id: 3, name: 'cat-three'}
+   ,{id: 4, name: 'cat-four'}
+   ,{id: 5, name: 'cat-five'}
+   ,{id: 6, name: 'cat-six'}
+   ,{id: 7, name: 'cat-luckyone'}
+   ,{id: 8, name: 'cat-eight'}
+   ,{id: 9, name: 'cat-nine'}
    ,{id: 10, name: 'cat-ten'}
 ]
 
 const background = [
-    {id: 01, name: 'bg-one'}
-   ,{id: 02, name: 'bg-two'}
-   ,{id: 03, name: 'bg-three'}
-   ,{id: 04, name: 'bg-four'}
-   ,{id: 05, name: 'bg-five'}
-   ,{id: 06, name: 'bg-six'}
-   ,{id: 07, name: 'bg-luckyone'}
-   ,{id: 08, name: 'bg-eight'}
-   ,{id: 09, name: 'bg-nine'}
+    {id: 1, name: 'bg-one'}
+   ,{id: 2, name: 'bg-two'}
+   ,{id: 3, name: 'bg-three'}
+   ,{id: 4, name: 'bg-four'}
+   ,{id: 5, name: 'bg-five'}
+   ,{id: 6, name: 'bg-six'}
+   ,{id: 7, name: 'bg-luckyone'}
+   ,{id: 8, name: 'bg-eight'}
+   ,{id: 9, name: 'bg-nine'}
    ,{id: 10, name: 'bg-ten'}
 ];
 
