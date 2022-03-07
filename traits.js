e2dff6dad46bf944bd609b5f8a5eab09ff88dd09
thyoondev <th.yoon.dev@gmail.com>
2022-03-07 09:22:37 +0900

    add traits

---
+++ traits.js
@@ -0,0 +1,25 @@
+const cat = [
+    {id: 01, name: 'cat-one'}
+   ,{id: 02, name: 'cat-two'}
+   ,{id: 03, name: 'cat-three'}
+   ,{id: 04, name: 'cat-four'}
+   ,{id: 05, name: 'cat-five'}
+   ,{id: 06, name: 'cat-six'}
+   ,{id: 07, name: 'cat-luckyone'}
+   ,{id: 08, name: 'cat-eight'}
+   ,{id: 09, name: 'cat-nine'}
+   ,{id: 10, name: 'cat-ten'}
+]
+
+const background = [
+    {id: 01, name: 'bg-one'}
+   ,{id: 02, name: 'bg-two'}
+   ,{id: 03, name: 'bg-three'}
+   ,{id: 04, name: 'bg-four'}
+   ,{id: 05, name: 'bg-five'}
+   ,{id: 06, name: 'bg-six'}
+   ,{id: 07, name: 'bg-luckyone'}
+   ,{id: 08, name: 'bg-eight'}
+   ,{id: 09, name: 'bg-nine'}
+   ,{id: 10, name: 'bg-ten'}
+];
