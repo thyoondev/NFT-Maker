e43c5e68529bc0f09a3b8399f8f0c8faa05e11e4
thyoondev <th.yoon.dev@gmail.com>
2022-03-07 09:32:02 +0900

    중복 검사후 이미지 생성하기

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const { cat, background } = require("./traits.js");
 const NUM_OF_CATS = 10;
 const NUM_OF_BACKGROUND = 10;
 
+let NFTs = [];
+
 // 난수 발생
 const fnRng = (limit) => {
     return Math.floor(Math.random() * limit);
 }
 
-let nftTobe = [];
-nftTobe.push(cat[fnRng(NUM_OF_CATS)].id);
+// 중복 검사후 생성
+const fnGenerateWithoutRedundancy = () => {
+    let nftTobe = [];
+    
+    nftTobe.push(fnCheckRareTrait(cat[fnRng(NUM_OF_CATS)].id));
+    nftTobe.push(background[fnRng(NUM_OF_BACKGROUND)].id);
+    
+    if (NFTs.length > 0) {
+        for (let i=0; i<NFTs.length; i++) {
+            if (JSON.stringify(NFTs[i]) === JSON.stringify(nftTobe)) {
+                return  null;
+            }
+        }
+    }
+    return nftTobe;
+}
